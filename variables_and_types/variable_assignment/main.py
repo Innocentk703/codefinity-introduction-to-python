f6e9598f183440b722faf9dee3560041a149f8af
Innocentk703 <>
2026-02-01 15:33:42 +0000

    Variable assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,14 @@
+
+print("Item :", item_name)
+print("Price per unit: $", item_price)
+print("Quantity in stock:", item_stock_quantity)
+print("In stock:", in_stock)
+
+
+# Testing
+print("Item : ", item_name
+print("Price per unit: $", item_price)
+print("$3.99")
+print("quantity in stock: ", item_stock_quantity)
+print("120")
+print("In stock:
